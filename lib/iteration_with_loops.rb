50824730753970b30e4740c74ddea447ba8f3e3e
Lauren Cunningham <lauren.busbee@gmail.com>
2020-07-14 01:56:15 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -5,7 +5,7 @@ def join_nested_strings(src)
   row_index = 0 
   while row_index < src.size do
     element_index = 0 
-    while element_index < [row_index][element_index].size do
+    while element_index < [row_index].size do
       if [row_index][element_index] == String
         single_value += src[row_index][element_index]
         end
